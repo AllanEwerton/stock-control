8397a333e7e9897683c454ec66a552c5cc851ad3
AllanEwerton <allanewerton24102004@gmail.com>
2025-03-11 17:24:06 -0300

    Adiciona modelos, controladores e migrações para categorias e produtos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PodutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('produtos', PodutoController::class);
