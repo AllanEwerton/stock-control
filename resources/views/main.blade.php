5b12f39dad6886d7edb594f7c63369a81d536e66
AllanEwerton <allanewerton24102004@gmail.com>
2025-03-18 00:51:20 -0300

    Adiciona páginas de login e cadastro, atualiza rotas e melhora estilos globais

---
+++ resources/views/main.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <!-- Importando estilos e scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body>
+        <!-- Cabeçalho -->
+        <header>
+            <div class="container">
+
+                <!-- Logo -->
+                <div class="logo">
+                    <a href="#">
+                        <img src="{{ asset('asset/images/log-white.png.png') }}" alt="Logo" class="w-28 h-auto">
+                    </a>
+                </div>
+
+                <!-- Navegação -->
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/login">Entrar</a></li>
+                        <li><a href="/cadastro">Cadastre-se</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <!-- Conteúdo dinâmico -->
+        <main>
+            @yield('content')
+        </main>
+
+        <!-- Rodapé -->
+        <footer>
+            <p >&copy; 2025 Sua Empresa. Todos os direitos reservados.</p>
+        </footer>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\PodutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+route::get('/login', function(){
+    return view('login.login');})->name('login');
+    route::get('/cadastro', function(){
+        return view('login.cadastro');})->name('cadastro');
+
 Route::resource('produtos', PodutoController::class);
